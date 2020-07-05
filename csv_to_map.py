40a58f749006e26ce75918b86cf8e9fe4ea475b5
TayyabMir056 <54217923+TayyabMir056@users.noreply.github.com>
2020-07-05 08:59:02 +0400

    Comments

--- csv_to_map.py
+++ csv_to_map.py
@@ -3,21 +3,24 @@ import json
 from IPython.display import display
 import arcgis
 from arcgis.gis import GIS,ContentManager
-from arcgis.mapping import WebMap, WebScene
+from arcgis.mapping import WebMap
 
+#Enter portal details
 gis= GIS(url="", username="", password ="")
 
-import os
-from arcgis.mapping import WebMap
-wm_Z3 = WebMap()
+wm_Z1 = WebMap()
 folderpath = r'M:\Planning\GIS\GIS Staff\Tayyab\Hussam\DSS per users\CSVs Per User\Zone 1'
 zone1Areas=os.listdir(folderpath)
+
+#Loop through all the csvs in the mentioned folder, and add them in the GIS as layers, publish them, and add them to the above created web map
 for areacsv in zone1Areas:
-    if areacsv!='.cekey':
+    if areacsv!='.cekey':#Ignore Lock files
         csv_file=folderpath+'\\'+areacsv
         csv_item= gis.content.add({'type':'CSV'},csv_file)
         lyr=csv_item.publish()
         wm_Z3.add_layer(lyr)
+        
+#Set Map properties and save.
 web_map_properties = {'title':'Zone 1 - GMT Distribution Substations Routine check MAP',
                      'snippet':'',
                      'tags':'MEDC'}
